893d6e88556bc444319c806a26096012e6132dda
Prashant <r56.prashant@gmail.com>
2024-04-14 21:48:22 +0545

    first commit

---
+++ src/main/java/com/example/atmConsole/AtmConsoleApplication.java
@@ -0,0 +1,13 @@
+package com.example.atmConsole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmConsoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtmConsoleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/atmConsole/AtmConsoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.atmConsole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmConsoleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
